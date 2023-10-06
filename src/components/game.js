dae45405496160c5615e53e0d028286a790d606a
Its-Tanay <tantiw1910@gmail.com>
2023-10-07 02:53:13 +0530

    Optimised images and used framer motion.

--- src/components/game.js
+++ src/components/game.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import { motion } from 'framer-motion';
 
 export default function Game({ random, handleImageClick }) {
+  const cardVariants = {
+    hidden: {
+      opacity: 0,
+      scale: 0.8,
+      rotate: -5,
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      rotate: 0,
+      transition: {
+        duration: 0.6,
+      },
+    },
+    hover: {
+      scale: 1.05,
+      rotate: 5,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    clicked: {
+      scale: 0.95,
+      opacity: 0.7,
+      rotate: 0,
+    },
+  };
+
   return (
     <div id="card-grid">
       {random.map((character) => (
-        <img
+        <motion.img
           key={character.id}
           src={character.image}
-          alt={`img${character.id}`} 
+          alt={`img${character.id}`}
           id={`char${character.id}`}
           onClick={() => handleImageClick(character.id)}
+          initial="hidden"
+          animate="visible"
+          whileHover="hover"
+          whileTap="clicked"
+          variants={cardVariants}
         />
       ))}
     </div>
